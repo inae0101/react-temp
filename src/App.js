6a4695985eac256d0764e36bb608665bd936265d
tubi55 <tubi55@nate.com>
2022-04-06 10:26:03 +0900

    react template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+  JSX : HTML문법과 비슷하게 동적으로 DOM을 생성해주는 문법 체계 (가상DOM: virtualDOM)
+  Component: JSX를 통해서 특정 기능을 하는 가상DOM구조를 재활용가능하게 함수형태로 컴포넌트
+*/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header>
+				<h1>Logo</h1>
+
+				<ul id='gnb'>
+					<li>menu</li>
+					<li>menu</li>
+					<li>menu</li>
+				</ul>
+			</header>
+
+			<figure>Visual</figure>
+
+			<section>
+				<article>content1</article>
+				<article>content2</article>
+				<article>content3</article>
+			</section>
+
+			<footer>footer</footer>
+		</div>
+	);
 }
 
 export default App;
